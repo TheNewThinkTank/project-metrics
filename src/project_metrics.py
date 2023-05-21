eb566139f146552024cf3c6d7c262413b1f93eb8
Gustav Rasmussen <gcr84@hotmail.com>
2023-05-21 17:39:53 +0200

    fix: hardcode username

--- src/project_metrics.py
+++ src/project_metrics.py
@@ -228,7 +228,7 @@ def main() -> None:
     platforms = {
         "github": {
             "username": "TheNewThinkTank",
-            "repos_url": f"https://api.github.com/users/{username}/repos",
+            "repos_url": "https://api.github.com/users/TheNewThinkTank/repos",
             "api_url": "https://api.github.com/repos/{owner}/{repo}",
             "access_token": os.environ["PROJECT_METRICS_GITHUB_ACCESS_TOKEN"],
             "stars": "stargazers_count",
@@ -236,7 +236,7 @@ def main() -> None:
         },
         "gitlab": {
             "username": "TheNewThinkTank",
-            "repos_url": f"https://gitlab.com/api/v4/users/{username}/projects?owned=true&visibility=public",
+            "repos_url": "https://gitlab.com/api/v4/users/TheNewThinkTank/projects?owned=true&visibility=public",
             "api_url": "https://gitlab.com/api/v4/projects/{owner}%2F{repo}",
             "access_token": os.environ["PROJECT_METRICS_GITLAB_ACCESS_TOKEN"],
             "stars": "star_count",
@@ -244,7 +244,7 @@ def main() -> None:
         },
         # "bitbucket": {
         #     "username": "Gustav_Collin_Rasmussen",
-        #     "repos_url": f"https://api.bitbucket.org/2.0/repositories/{username}",
+        #     "repos_url": "https://api.bitbucket.org/2.0/repositories/Gustav_Collin_Rasmussen",
         #     "api_url": "https://api.bitbucket.org/2.0/repositories/{owner}/{repo}",
         #     "access_token": os.environ["PROJECT_METRICS_BITBUCKET_ACCESS_TOKEN"],
         #     "stars": "",

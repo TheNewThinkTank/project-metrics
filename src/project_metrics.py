12ba84497878811b8e6ea48802ff29ad91adb7dc
Gustav Rasmussen <gcr84@hotmail.com>
2023-05-21 15:56:26 +0200

    chore: skip get_metrics

--- src/project_metrics.py
+++ src/project_metrics.py
@@ -229,18 +229,21 @@ def main() -> None:
     # Define the API endpoints and access tokens for each platform
     platforms = {
         "github": {
+            "username": "TheNewThinkTank",
             "api_url": "https://api.github.com/repos/{owner}/{repo}",
             "access_token": os.environ["PROJECT_METRICS_GITHUB_ACCESS_TOKEN"],
             "stars": "stargazers_count",
             "url": "html_url"
         },
         "gitlab": {
+            "username": "TheNewThinkTank",
             "api_url": "https://gitlab.com/api/v4/projects/{owner}%2F{repo}",
             "access_token": os.environ["PROJECT_METRICS_GITLAB_ACCESS_TOKEN"],
             "stars": "star_count",
             "url": "web_url"
         },
         # "bitbucket": {
+        #     "username": "Gustav_Collin_Rasmussen",
         #     "api_url": "https://api.bitbucket.org/2.0/repositories/{owner}/{repo}",
         #     "access_token": os.environ["PROJECT_METRICS_BITBUCKET_ACCESS_TOKEN"],
         #     "stars": "",

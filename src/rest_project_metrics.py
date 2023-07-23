d302fe42f952329877dd958d2d9a824c23690d7a
Gustav Rasmussen <gcr84@hotmail.com>
2023-07-23 16:17:19 +0200

    refactor: all get repo calls into get_repos module

--- src/rest_project_metrics.py
+++ src/rest_project_metrics.py
@@ -7,116 +7,7 @@ from pprint import pprint as pp
 import requests
 
 from config import platforms
-from get_repos import get_repos
-
-
-def get_repo_info(platforms: dict, repo) -> dict | None:
-    """Get the information for a single repository.
-
-    :param platforms: _description_
-    :type platforms: _type_
-    :param repo: _description_
-    :type repo: _type_
-    :return: _description_
-    :rtype: dict
-    """
-
-    # Get the API endpoint and access token for the repository's platform
-    platform = platforms[repo["platform"]]
-    api_url = platform["api_url"].format(owner=repo["owner"], repo=repo["name"])
-    headers = {"Authorization": f"Bearer {platform['access_token']}"}
-
-    # Make API requests to get the information for the repository
-    response = requests.get(api_url, headers=headers)
-    data = response.json()
-
-    # pp(data)
-    # for k in sorted(data.keys()):
-    #     print(k)
-
-    # pp(data.keys())
-    # print(type((data)))
-    # print("name" in data.keys())
-    # print(data["name"])
-
-    try:
-        # print("Parsing the information from the API response")
-        info = {
-            "name": data["name"],
-            # "owner": data["owner"]["login"],
-            "platform": repo["platform"],
-            "url": data[platform["url"]],
-            "description": data["description"],
-            "stars": data[platform["stars"]],
-            "creation_date": data["created_at"],  # datetime.datetime.strptime(data["created_at"], "%Y-%m-%dT%H:%M:%SZ"),
-            # "commits": 0,
-            # "open_issues": 0,
-            # "closed_issues": 0,
-            # "repo_size": data["size"],
-            # "health_status": "",
-            # "languages": []
-        }
-    except KeyError:
-        print(f"Error: could not get information for repository {repo}")
-        return None
-
-    # if repo["platform"] == "github":
-    #     info["commits"] = data["default_branch"]
-    #     issues_url = api_url + "/issues?state=all"
-    # elif repo["platform"] == "gitlab":
-    #     info["commits"] = data["default_branch"]
-    #     issues_url = api_url + "/issues?state=all"
-    # elif repo["platform"] == "bitbucket":
-    #     response = requests.get(data["links"]["branches"]["href"], headers=headers)
-    #     branch_data = response.json()
-    #     default_branch = branch_data["values"][0]["name"]
-    #     info["commits"] = default_branch
-    #     issues_url = api_url + "/issues?q=state%20in%20(%22new%22,%22open%22,%22resolved%22,%22on_hold%22,%22invalid%22,%22duplicate%22)&fields=-votes,-watchers_count"
-
-    # response = requests.get(issues_url, headers=headers)
-    # data = response.json()
-    # info["open_issues"] = data["size"]
-    # info["closed_issues"] = len([issue for issue in data["values"] if issue["state"] == "closed"])
-
-    # # Calculate the health status of the repository based on the number of open issues
-    # if info["open_issues"] == 0:
-    #     info["health_status"] = "good"
-    # elif info["open_issues"] < 10:
-    #     info["health_status"] = "fair"
-    # else:
-    #     info["health_status"] = "poor"
-
-    # # Get the languages used in the repository
-    # response = requests.get(api_url + "/languages", headers=headers)
-    # data = response.json()
-    # for language, bytes_of_code in data.items():
-    #     info["languages"].append({"name": language, "bytes_of_code": bytes_of_code})
-
-    return info
-
-
-def print_repo_info(repo_info: dict) -> None:
-    """_summary_
-
-    :param repo_info: _description_
-    :type repo_info: dict
-    """
-
-    print(f"Name: {repo_info['name']}")
-    # print(f"Owner: {repo_info['owner']}")
-    print(f"Platform: {repo_info['platform']}")
-    print(f"URL: {repo_info['url']}")
-    print(f"Description: {repo_info['description']}")
-    print(f"Stars: {repo_info['stars']}")
-    print(f"Creation Date: {repo_info['creation_date']}")
-    # print(f"Commits: {repo_info['commits']}")
-    # print(f"Open Issues: {repo_info['open_issues']}")
-    # print(f"Closed Issues: {repo_info['closed_issues']}")
-    # print(f"Repo Size: {repo_info['repo_size']} bytes")
-    # print(f"Health Status: {repo_info['health_status']}")
-    # print("Languages:")
-    # for language in repo_info["languages"]:
-    #     print(f"\t{language['name']}: {language['bytes_of_code']} bytes")
+from get_repos import get_all_repos, get_repo_info  # , get_repos, print_repo_info
 
 
 def repo_missing_descriptions(repo_info: dict) -> bool:
@@ -129,23 +20,6 @@ def repo_missing_descriptions(repo_info: dict) -> bool:
     """
 
     return False if repo_info["description"] else True
-
-
-def get_all_repos(platforms: dict) -> list:
-
-    # print(get_gl_repos())
-    # get_bb_repos()
-
-    gh_repos = get_repos("github")
-    gl_repos = get_repos("gitlab")
-    # bb_repos = get_repos("bitbucket")
-
-    all_repos = []
-    all_repos.extend(gh_repos)
-    all_repos.extend(gl_repos)
-    # all_repos.extend(bb_repos)
-
-    return all_repos
 
 
 def get_repos_wo_desc(platforms: dict, all_repos: list) -> list:

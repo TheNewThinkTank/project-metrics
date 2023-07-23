64b373f9cd854355b34f1f9b07d88e0475ff6759
Gustav Rasmussen <gcr84@hotmail.com>
2023-07-23 22:03:27 +0200

    test: concatenate badge and tmp content

--- src/REST/add_repo_size_badge.py
+++ src/REST/add_repo_size_badge.py
@@ -52,9 +52,33 @@ size = get_repo_size(username, repository)
 print(f"{size = }")
 
 
+size_badge = f"[![GitHub repo size](https://img.shields.io/github/repo-size/TheNewThinkTank/{repository}?style=flat&logo=github&logoColor=whitesmoke&label=Repo%20Size)](https://github.com/TheNewThinkTank/{repository}/archive/refs/heads/main.zip)"
+
+
+
 contents = repo.get_contents("README.md", ref="master")
 
-repo.update_file(contents.path, "Chore: update README", str(size), contents.sha, branch="master")
+tmp = """
+# AACT-Analysis
+Visual exploration of the AACT dataset for 2020
+
+CTTI has aggregated and restructured publicly available ClinicalTrials.gov data and made it available here as a relational database to facilitate analysis of the complete set of trials. This database is the Aggregate Analysis of ClincalTrials.gov (AACT).
+
+By combining the two datasets "conditions" and "location_countries" inside the AACT database,
+this project shows not only the most prevalent conditions globally but also per nation,
+filtered by most represented countries (here US is shown). These plots are generated from date: 20200601.
+
+
+![](img/most_common_conditions_2020.png)
+
+![](img/most_common_conditions_us_2020.png)
+
+![](img/obesity_us.png)
+"""
+
+
+
+repo.update_file(contents.path, "Chore: update README", size_badge + tmp, contents.sha, branch="master")
 
 
 # with open(os.path.join(repo_path, 'README.md'), 'r') as rf:

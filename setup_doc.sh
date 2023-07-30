4af85fc1751b970b98b9426fcef7f9d615713c22
Gustav Rasmussen <gcr84@hotmail.com>
2023-07-30 21:15:30 +0200

    feat: run sphinx-quickstart

---
+++ setup_doc.sh
@@ -0,0 +1,17 @@
+#!/opt/homebrew/bin/bash
+
+# Setup Sphinx
+
+# quick start:
+# https://www.sphinx-doc.org/en/master/usage/quickstart.html
+
+mkdir docs && cd docs
+
+# Project_name="project-metrics"
+# Author_name="Gustav Collin Rasmussen"
+# Project_release="1.0.0"
+sphinx-quickstart
+
+# make clean
+# sphinx-apidoc -o ./source ../src
+# make html

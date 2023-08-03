22a9b79bea98340b4ac27fd3d7cb55644f1e8b92
Gustav Rasmussen <gcr84@hotmail.com>
2023-08-04 00:44:16 +0200

    fix: remove lint

--- test/test_get_badge.py
+++ test/test_get_badge.py
@@ -8,34 +8,34 @@ from src.get_badge import get_badge
 testdata = [
     ("fitness-tracker",
       "size_badge",
-    {
+     {
         "label": "GitHub repo size",
         "value": [
             "https://img.shields.io/github/repo-size/TheNewThinkTank/fitness-tracker?style=flat&logo=github&logoColor=whitesmoke&label=Repo%20Size",
             "https://github.com/TheNewThinkTank/fitness-tracker/archive/refs/heads/main.zip",
         ]
-    }
+     }
     ),
     ("world-maps",
       "ci_badge",
-    {
+     {
         "label": "CI",
         "value": "https://github.com/TheNewThinkTank/world-maps/actions/workflows/wf.yml/badge.svg",
-    }
+     }
     ),
     ("workout-generator",
       "codecov_badge",
-    {
+     {
         "label": "codecov",
         "value": [
             "https://codecov.io/gh/TheNewThinkTank/workout-generator/branch/main/graph/badge.svg",
             "https://codecov.io/gh/TheNewThinkTank/workout-generator)"
         ]
-    }
+     }
     ),
 ]
 
 
 @pytest.mark.parametrize("repo,badge,expected", testdata)
-def test_get_badge(repo,badge,expected):
+def test_get_badge(repo, badge, expected):
     assert get_badge(repo, badge) == expected

6af1d3a6efd19f7c1bedb40eade47faabfe300b0
Gustav Rasmussen <gcr84@hotmail.com>
2023-08-04 00:50:54 +0200

    style: format using black

--- test/test_get_badge.py
+++ test/test_get_badge.py
@@ -6,32 +6,35 @@ import pytest
 from src.get_badge import get_badge
 
 testdata = [
-    ("fitness-tracker",
-      "size_badge",
-     {
-        "label": "GitHub repo size",
-        "value": [
-            "https://img.shields.io/github/repo-size/TheNewThinkTank/fitness-tracker?style=flat&logo=github&logoColor=whitesmoke&label=Repo%20Size",
-            "https://github.com/TheNewThinkTank/fitness-tracker/archive/refs/heads/main.zip",
-        ]
-     }
+    (
+        "fitness-tracker",
+        "size_badge",
+        {
+            "label": "GitHub repo size",
+            "value": [
+                "https://img.shields.io/github/repo-size/TheNewThinkTank/fitness-tracker?style=flat&logo=github&logoColor=whitesmoke&label=Repo%20Size",
+                "https://github.com/TheNewThinkTank/fitness-tracker/archive/refs/heads/main.zip",
+            ],
+        },
     ),
-    ("world-maps",
-      "ci_badge",
-     {
-        "label": "CI",
-        "value": "https://github.com/TheNewThinkTank/world-maps/actions/workflows/wf.yml/badge.svg",
-     }
+    (
+        "world-maps",
+        "ci_badge",
+        {
+            "label": "CI",
+            "value": "https://github.com/TheNewThinkTank/world-maps/actions/workflows/wf.yml/badge.svg",
+        },
     ),
-    ("workout-generator",
-      "codecov_badge",
-     {
-        "label": "codecov",
-        "value": [
-            "https://codecov.io/gh/TheNewThinkTank/workout-generator/branch/main/graph/badge.svg",
-            "https://codecov.io/gh/TheNewThinkTank/workout-generator)"
-        ]
-     }
+    (
+        "workout-generator",
+        "codecov_badge",
+        {
+            "label": "codecov",
+            "value": [
+                "https://codecov.io/gh/TheNewThinkTank/workout-generator/branch/main/graph/badge.svg",
+                "https://codecov.io/gh/TheNewThinkTank/workout-generator)",
+            ],
+        },
     ),
 ]
 

68fc8cd71099eadfca149c897e231e61041c3783
Gustav Rasmussen <gcr84@hotmail.com>
2023-08-04 00:38:41 +0200

    test: add parametrized unittests

--- test/test_get_badge.py
+++ test/test_get_badge.py
@@ -1,28 +1,41 @@
+"""_summary_
+"""
+
+import pytest
 
 from src.get_badge import get_badge
 
-repo_names = [
-    "fitness-tracker",
-    "world-maps",
-    "workout-generator",
-]
-
-badge_names = [
-    "size_badge",
-    "ci_badge",
-    "codecov_badge",
-]
-
-
-def test_get_badge():
-
-    repo_name = repo_names[0]
-    badge_name = badge_names[0]
-
-    assert get_badge(repo_name, badge_name) == {
+testdata = [
+    ("fitness-tracker",
+      "size_badge",
+    {
         "label": "GitHub repo size",
         "value": [
             "https://img.shields.io/github/repo-size/TheNewThinkTank/fitness-tracker?style=flat&logo=github&logoColor=whitesmoke&label=Repo%20Size",
             "https://github.com/TheNewThinkTank/fitness-tracker/archive/refs/heads/main.zip",
         ]
     }
+    ),
+    ("world-maps",
+      "ci_badge",
+    {
+        "label": "CI",
+        "value": "https://github.com/TheNewThinkTank/world-maps/actions/workflows/wf.yml/badge.svg",
+    }
+    ),
+    ("workout-generator",
+      "codecov_badge",
+    {
+        "label": "codecov",
+        "value": [
+            "https://codecov.io/gh/TheNewThinkTank/workout-generator/branch/main/graph/badge.svg",
+            "https://codecov.io/gh/TheNewThinkTank/workout-generator)"
+        ]
+    }
+    ),
+]
+
+
+@pytest.mark.parametrize("repo,badge,expected", testdata)
+def test_get_badge(repo,badge,expected):
+    assert get_badge(repo, badge) == expected

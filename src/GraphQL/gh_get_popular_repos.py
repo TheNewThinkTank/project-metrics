3e9458cc18dc86b4c19d33d40f5f24b3d602ff44
Gustav Rasmussen <gcr84@hotmail.com>
2023-08-04 21:37:17 +0200

    feat: add job to wf

--- src/GraphQL/gh_get_popular_repos.py
+++ src/GraphQL/gh_get_popular_repos.py
@@ -5,7 +5,7 @@ import os
 from pprint import pprint as pp
 import requests  # type: ignore
 
-from make_md_table import table  # type: ignore
+from util.make_md_table import table  # type: ignore
 from save_file_to_github import save_file_to_github  # type: ignore
 
 
@@ -20,10 +20,10 @@ def fetch_top_repos(username: str, token: str) -> list:
     :rtype: list
     """
 
-    url = 'https://api.github.com/graphql'
-    headers = {'Authorization': f'bearer {token}'}
+    url = "https://api.github.com/graphql"
+    headers = {"Authorization": f"bearer {token}"}
 
-    query = '''
+    query = """
     query ($login: String!, $limit: Int!) {
       user(login: $login) {
         repositories(first: $limit, orderBy: {field: STARGAZERS, direction: DESC}) {
@@ -38,34 +38,36 @@ def fetch_top_repos(username: str, token: str) -> list:
         }
       }
     }
-    '''
+    """
 
-    variables = {'login': username, 'limit': 10}
-    response = requests.post(url, json={'query': query, 'variables': variables}, headers=headers)
+    variables = {"login": username, "limit": 10}
+    response = requests.post(
+        url, json={"query": query, "variables": variables}, headers=headers
+    )
 
     if response.status_code == 200:
         data = response.json()
-        repositories = data['data']['user']['repositories']['edges']
+        repositories = data["data"]["user"]["repositories"]["edges"]
         popular_repos = [
             {
-                "name": repo['node']['name'],
-                "stars": repo['node']['stargazers']['totalCount']
+                "name": repo["node"]["name"],
+                "stars": repo["node"]["stargazers"]["totalCount"],
             }
             for repo in repositories
-            ]
+        ]
         pp(popular_repos)
         return popular_repos
     else:
-        print(f'Request failed with status code {response.status_code}')
+        print(f"Request failed with status code {response.status_code}")
         print(response.text)
         return []
 
 
 def main() -> None:
     token = os.environ["FG_GITHUB_ACCESS_TOKEN"]
-    popular_repos = fetch_top_repos('TheNewThinkTank', token)
-    repo_name = 'project-metrics'
-    file_path = 'query-results/popular_repos.md'
+    popular_repos = fetch_top_repos("TheNewThinkTank", token)
+    repo_name = "project-metrics"
+    file_path = "query-results/popular_repos.md"
     file_content = table(popular_repos)
     save_file_to_github(repo_name, file_path, file_content)
 
--- src/GraphQL/gh_group_by_lang.py
+++ src/GraphQL/gh_group_by_lang.py
@@ -6,7 +6,7 @@ from pprint import pprint as pp
 import requests  # type: ignore
 
 from save_file_to_github import save_file_to_github  # type: ignore
-from make_md_table import table_from_nested  # type: ignore
+from util.make_md_table import table_from_nested  # type: ignore
 
 
 def group_repos_by_language(username: str, token: str) -> list:
@@ -20,9 +20,9 @@ def group_repos_by_language(username: str, token: str) -> list:
     :rtype: list
     """
 
-    url = 'https://api.github.com/graphql'
-    headers = {'Authorization': f'bearer {token}'}
-    query = '''
+    url = "https://api.github.com/graphql"
+    headers = {"Authorization": f"bearer {token}"}
+    query = """
     query ($login: String!) {
       user(login: $login) {
         repositories(first: 100, orderBy: {field: NAME, direction: ASC}) {
@@ -35,20 +35,22 @@ def group_repos_by_language(username: str, token: str) -> list:
         }
       }
     }
-    '''
-    variables = {'login': username}
-    response = requests.post(url, json={'query': query, 'variables': variables}, headers=headers)
+    """
+    variables = {"login": username}
+    response = requests.post(
+        url, json={"query": query, "variables": variables}, headers=headers
+    )
 
     if response.status_code == 200:
         data = response.json()
-        repositories = data['data']['user']['repositories']['nodes']
+        repositories = data["data"]["user"]["repositories"]["nodes"]
         language_groups = dict()  # type: ignore
 
         for repo in repositories:
-            name = repo['name']
-            language = repo['primaryLanguage']
+            name = repo["name"]
+            language = repo["primaryLanguage"]
             if language is not None:
-                language_name = language['name']
+                language_name = language["name"]
                 if language_name in language_groups:
                     language_groups[language_name].append(name)
                 else:
@@ -59,17 +61,17 @@ def group_repos_by_language(username: str, token: str) -> list:
         return [language_groups]
 
     else:
-        print(f'Request failed with status code {response.status_code}')
+        print(f"Request failed with status code {response.status_code}")
         print(response.text)
         return []
 
 
 def main() -> None:
-    repo_name = 'project-metrics'
-    lang_repos = group_repos_by_language('TheNewThinkTank',
-                                         os.environ["FG_GITHUB_ACCESS_TOKEN"]
-                                         )
-    file_path = 'query-results/group_by_lang.md'
+    repo_name = "project-metrics"
+    lang_repos = group_repos_by_language(
+        "TheNewThinkTank", os.environ["FG_GITHUB_ACCESS_TOKEN"]
+    )
+    file_path = "query-results/group_by_lang.md"
     file_content = table_from_nested(lang_repos)
 
     save_file_to_github(repo_name, file_path, file_content)

1b80520b290abdc082bb2bf907ec5313b86732f3
Gustav Rasmussen <gcr84@hotmail.com>
2023-08-04 17:24:28 +0200

    test: unit test get_gh_repos

---
+++ test/test_get_gh_repos.py
@@ -0,0 +1,51 @@
+import os
+from unittest.mock import Mock, patch
+
+from github import Auth, Github
+
+from src.util.get_gh_repos import get_gh_repos
+
+
+def test_get_gh_repos_with_default_values():
+    # Arrange
+    expected_username = "TheNewThinkTank"
+    expected_access_token = "mocked_access_token"
+    repo_mock = Mock()
+    user_mock = Mock()
+    user_mock.get_repos.return_value = repo_mock
+
+    github_mock = Mock()
+    github_mock.get_user.return_value = user_mock
+
+    with patch.dict(
+        os.environ, {"PROJECT_METRICS_GITHUB_ACCESS_TOKEN": expected_access_token}
+    ):
+        # Act
+        result = get_gh_repos()
+
+    # Assert
+    assert result == repo_mock
+    github_mock.get_user.assert_called_once_with(expected_username)
+    Auth.Token.assert_called_once_with(expected_access_token)
+    Github.assert_called_once_with(auth=Auth.Token())
+
+
+def test_get_gh_repos_with_custom_values():
+    # Arrange
+    custom_username = "CustomUser"
+    custom_access_token = "custom_access_token"
+    repo_mock = Mock()
+    user_mock = Mock()
+    user_mock.get_repos.return_value = repo_mock
+
+    github_mock = Mock()
+    github_mock.get_user.return_value = user_mock
+
+    # Act
+    result = get_gh_repos(username=custom_username, access_token=custom_access_token)
+
+    # Assert
+    assert result == repo_mock
+    github_mock.get_user.assert_called_once_with(custom_username)
+    Auth.Token.assert_called_once_with(custom_access_token)
+    Github.assert_called_once_with(auth=Auth.Token())

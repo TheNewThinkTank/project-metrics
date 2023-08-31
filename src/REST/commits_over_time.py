5bd72f7496de3a171d49df2b4eb0a1c1e58405e6
Gustav Rasmussen <gcr84@hotmail.com>
2023-08-31 22:44:49 +0200

    feat: improve data structure

--- src/REST/commits_over_time.py
+++ src/REST/commits_over_time.py
@@ -10,31 +10,31 @@ from save_file_to_github import save_file_to_github  # type: ignore
 
 owner = "TheNewThinkTank"
 repo = "fitness-tracker"  # "N-body-simulations"
-
 url = f"https://api.github.com/repos/{owner}/{repo}/commits"
 response = requests.get(url)
 
 if response.status_code == 200:
     commits_data = response.json()
-
-    commit_dates = []
-    commit_counts = []
-
+    commit_activity = {}
     for commit in commits_data:
         commit_date = datetime.strptime(
             commit["commit"]["committer"]["date"], "%Y-%m-%dT%H:%M:%SZ"
-        )
-        commit_dates.append(commit_date)
-        commit_counts.append(1)  # Each commit counts as 1
+        ).date()
+        if commit_date in commit_activity:
+            commit_activity[commit_date] += 1
+        else:
+            commit_activity[commit_date] = 1
+
+    sorted_dates = sorted(commit_activity.keys())
+    commit_counts = [commit_activity[date] for date in sorted_dates]
 
     plt.figure(figsize=(10, 6))
-    plt.plot(commit_dates, commit_counts, marker="o")
-    plt.title(f"Commit Activity - {repo}")
+    plt.plot(sorted_dates, commit_counts, marker="o")
+    plt.title(f"Commit frequency - {repo}")
     plt.xlabel("Date")
     plt.ylabel("Number of Commits")
     plt.xticks(rotation=45)
     plt.tight_layout()
-    # plt.show()
     local_file_path = f"imgs/commits_over_time_{repo}.png"
     plt.savefig(local_file_path)
     plt.close()

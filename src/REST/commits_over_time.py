0b1b4cec709cebaa7a4a5318547a62532598c7bc
Gustav Rasmussen <gcr84@hotmail.com>
2023-08-31 23:09:03 +0200

    feat: delete old plots. add pagination

--- src/REST/commits_over_time.py
+++ src/REST/commits_over_time.py
@@ -11,35 +11,48 @@ from save_file_to_github import save_file_to_github  # type: ignore
 owner = "TheNewThinkTank"
 repo = "project-metrics"  # "fitness-tracker"  # "N-body-simulations"
 url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-response = requests.get(url)
 
-if response.status_code == 200:
-    commits_data = response.json()
-    commit_activity = dict()  # type: ignore
-    for commit in commits_data:
-        commit_date = datetime.strptime(
-            commit["commit"]["committer"]["date"], "%Y-%m-%dT%H:%M:%SZ"
-        ).date()
-        if commit_date in commit_activity:
-            commit_activity[commit_date] += 1
-        else:
-            commit_activity[commit_date] = 1
+commit_activity = dict()  # type: ignore
 
-    sorted_dates = sorted(commit_activity.keys())
-    commit_counts = [commit_activity[date] for date in sorted_dates]
+page = 1
+while True:
+    response = requests.get(
+        url, params={"page": page, "per_page": 100}
+    )  # Fetch 100 commits per page
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(sorted_dates, commit_counts, marker="o")
-    plt.title(f"Commit frequency - {repo}")
-    plt.xlabel("Date")
-    plt.ylabel("Number of Commits")
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    local_file_path = f"imgs/commits_over_time_{repo}.png"
-    plt.savefig(local_file_path)
-    plt.close()
-    with open(local_file_path, "rb") as file:
-        content = file.read()
-    save_file_to_github("project-metrics", local_file_path, content)
-else:
-    print(f"Failed to fetch commit history. Status code: {response.status_code}")
+    if response.status_code == 200:
+        commits_data = response.json()
+
+        if not commits_data:  # No more commits, break the loop
+            break
+
+        for commit in commits_data:
+            commit_date = datetime.strptime(
+                commit["commit"]["committer"]["date"], "%Y-%m-%dT%H:%M:%SZ"
+            ).date()
+            if commit_date in commit_activity:
+                commit_activity[commit_date] += 1
+            else:
+                commit_activity[commit_date] = 1
+
+        page += 1  # Move to the next page
+    else:
+        print(f"Failed to fetch commit history. Status code: {response.status_code}")
+        break
+
+sorted_dates = sorted(commit_activity.keys())
+commit_counts = [commit_activity[date] for date in sorted_dates]
+
+plt.figure(figsize=(10, 6))
+plt.plot(sorted_dates, commit_counts, marker="o")
+plt.title(f"Commit frequency - {repo}")
+plt.xlabel("Date")
+plt.ylabel("Number of Commits")
+plt.xticks(rotation=45)
+plt.tight_layout()
+local_file_path = f"imgs/commits_over_time_{repo}.png"
+plt.savefig(local_file_path)
+plt.close()
+with open(local_file_path, "rb") as file:
+    content = file.read()
+save_file_to_github("project-metrics", local_file_path, content)

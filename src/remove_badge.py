e9d6efc8cc14f02ad8b144f16f95dfef8aa1369a
Gustav Rasmussen <gcr84@hotmail.com>
2023-09-07 09:26:20 +0200

    chore: format docstring

--- src/remove_badge.py
+++ src/remove_badge.py
@@ -1,28 +1,22 @@
 """Given one or more badges to remove,
 and one or more repos to remove them from,
 loop through all the repos and apply the removals.
-
 -------------------------------------------------------------------------------------
-
 EXAMPLES:
 
 Cleanup cross-over badges, i.e. badges belonging to a different repo (e.g. repo: AACT-Analysis).
 
 badges to remove:
 
-
 ![CI]
 (https://github.com/TheNewThinkTank/AACT-Analysis/actions/workflows/wf.yml/badge.svg)
-
 
 [![GitHub repo size]
 (https://img.shields.io/github/repo-size/TheNewThinkTank/AACT-Analysis?style=flat&logo=github&logoColor=whitesmoke&label=Repo%20Size)]
 (https://github.com/TheNewThinkTank/AACT-Analysis/archive/refs/heads/main.zip)
 
-
 test on a few repos
 and check that repo: 'AACT-Analysis' is unaffected.
-
 -------------------------------------------------------------------------------------
 
 TODO: perform above tasks in parallel.

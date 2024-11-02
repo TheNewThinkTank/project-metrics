023abec8d5c3ba0725acc322c9a0e71a3929eec7
Gustav Rasmussen <gcr84@hotmail.com>
2024-11-02 22:26:40 +0100

    test: remove 3 util modules

--- src/util/csv_to_md.py
+++ src/util/csv_to_md.py
@@ -1,44 +1,44 @@
-import csv
+# import csv
 
 
-def csv_to_md(in_file: str) -> None:
-    """Create a Markdown file from a csv file.
+# def csv_to_md(in_file: str) -> None:
+#     """Create a Markdown file from a csv file.
 
-    :param in_file: path to csv file
-    :type in_file: str
-    """
+#     :param in_file: path to csv file
+#     :type in_file: str
+#     """
 
-    out_file = in_file.replace(".csv", ".md")
-    csv_dict = csv.DictReader(open(in_file, encoding="UTF-8"), delimiter=",")
-    list_of_rows = [dict_row for dict_row in csv_dict]
-    header = list(list_of_rows[0].keys())
+#     out_file = in_file.replace(".csv", ".md")
+#     csv_dict = csv.DictReader(open(in_file, encoding="UTF-8"), delimiter=",")
+#     list_of_rows = [dict_row for dict_row in csv_dict]
+#     header = list(list_of_rows[0].keys())
 
-    md_string = " | "
-    for h in header:
-        md_string += h + " |"
+#     md_string = " | "
+#     for h in header:
+#         md_string += h + " |"
 
-    md_string += "\n |"
+#     md_string += "\n |"
 
-    for _ in range(len(header)):
-        md_string += "--- | "
+#     for _ in range(len(header)):
+#         md_string += "--- | "
 
-    md_string += "\n"
+#     md_string += "\n"
 
-    for row in list_of_rows:
-        md_string += " | "
-        for h in header:
-            md_string += row[h] + " | "
-        md_string += "\n"
+#     for row in list_of_rows:
+#         md_string += " | "
+#         for h in header:
+#             md_string += row[h] + " | "
+#         md_string += "\n"
 
-    with open(out_file, "w", encoding="UTF-8") as wf:
-        wf.write(md_string)
-
-
-def main() -> None:
-    # csv_to_md("popular_repos.csv")
-    # csv_to_md("repos_wo_desc.csv")
-    ...
+#     with open(out_file, "w", encoding="UTF-8") as wf:
+#         wf.write(md_string)
 
 
-if __name__ == "__main__":
-    main()
+# def main() -> None:
+#     # csv_to_md("popular_repos.csv")
+#     # csv_to_md("repos_wo_desc.csv")
+#     ...
+
+
+# if __name__ == "__main__":
+#     main()
--- src/util/txt_to_csv.py
+++ src/util/txt_to_csv.py
@@ -1,31 +1,31 @@
-"""_summary_
-"""
+# """_summary_
+# """
 
-import pandas as pd  # type: ignore
+# import pandas as pd  # type: ignore
 
-# TODO: replace single with double quotes
-# TODO: replace None values with double quotes
-
-
-def txt_to_csv(in_file: str, sep: str = ",") -> None:
-    """Create csv file from txt using Pandas.
-
-    :param in_file: name of txt file
-    :type in_file: str
-    :param sep: _description_, defaults to ','
-    :type sep: str, optional
-    """
-
-    df = pd.read_csv(in_file, sep=sep)
-
-    df.to_csv(in_file.split(".")[0] + ".csv", index=False)
+# # TODO: replace single with double quotes
+# # TODO: replace None values with double quotes
 
 
-def main() -> None:
-    # txt_to_csv('popular_repos.txt')
-    # txt_to_csv('repos_wo_desc.txt')
-    ...
+# def txt_to_csv(in_file: str, sep: str = ",") -> None:
+#     """Create csv file from txt using Pandas.
+
+#     :param in_file: name of txt file
+#     :type in_file: str
+#     :param sep: _description_, defaults to ','
+#     :type sep: str, optional
+#     """
+
+#     df = pd.read_csv(in_file, sep=sep)
+
+#     df.to_csv(in_file.split(".")[0] + ".csv", index=False)
 
 
-if __name__ == "__main__":
-    main()
+# def main() -> None:
+#     # txt_to_csv('popular_repos.txt')
+#     # txt_to_csv('repos_wo_desc.txt')
+#     ...
+
+
+# if __name__ == "__main__":
+#     main()

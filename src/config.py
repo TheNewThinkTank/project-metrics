ee09d52123bcf42ba8d0dddbeb75e06947cee666
Gustav Rasmussen <gcr84@hotmail.com>
2025-01-31 14:35:20 +0100

    test: update config

--- src/config.py
+++ src/config.py
@@ -7,23 +7,30 @@ settings = Dynaconf(
         settings_files=[".config/settings.toml"],
         # envvar_prefix="DYNACONF",
         environments=True,
-        default_env="default",
-        # load_dotenv=True,
+        # default_env="default",
+        load_dotenv=True,
         # envvar_for_dynaconf=".env",
     )
 
-settings.setenv("default")  # Ensure the correct environment is active
-
+# settings.setenv("default")  # Ensure the correct environment is active
 # settings.validators.validate()
-settings = settings.as_dict()
+# settings = settings.as_dict()
 
 
 def main() -> None:
 
     from pprint import pprint as pp
 
-    pp(settings)
+    # pp(settings)
+    # pp(settings.platforms.github.badges)  # ['PLATFORMS']['github']['badges'])
 
+    # pp(settings.platforms.github.badges.size_badge)
+
+    pp(settings.platforms.github.badges.size_badge.label)
+    pp(settings.platforms.github.badges.size_badge.value)
+
+    # pp(settings.platforms.github.badges.ci_badge)
+    # pp(settings.platforms.github.badges.codecov_badge)
 
 if __name__ == "__main__":
     main()
--- src/get_badge.py
+++ src/get_badge.py
@@ -1,7 +1,6 @@
 """_summary_
 """
 
-# from src.config import gh_badges  # type: ignore
 from src.config import settings  # type: ignore
 
 
@@ -19,16 +18,16 @@ def get_badge(repo_name: str, badge_name: str):
     # avoid modifying the original gh_badges dictionary, by making a copy
     # badge = gh_badges[badge_name].copy()
 
-    github_badges = settings['PLATFORMS']['github']['badges']
-    badge = github_badges[badge_name].copy()
+    github_badges = settings.platforms.github.badges  # settings['PLATFORMS']['github']['badges']
+    badge = github_badges.badge_name.copy()
 
-    if isinstance(badge["value"], str):  # type: ignore
-        badge["value"] = badge["value"].replace("{repo}", repo_name)  # type: ignore
+    if isinstance(badge.value, str):  # type: ignore
+        badge.value = badge.value.replace("{repo}", repo_name)  # type: ignore
 
-    elif isinstance(badge["value"], list):  # type: ignore
-        badge["value"] = [
+    elif isinstance(badge.value, list):  # type: ignore
+        badge.value = [
             url.replace("{repo}", repo_name)  # type: ignore
-            for url in badge["value"]  # type: ignore
+            for url in badge.value  # type: ignore
         ]  # type: ignore
 
     # print(f"badge from get_badge: {badge}")
@@ -36,10 +35,11 @@ def get_badge(repo_name: str, badge_name: str):
     return badge
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     from pprint import pprint as pp
+    from pprint import pprint as pp
 
-#     # pp(settings)
-#     pp(settings['PLATFORMS']['github']['badges']['size_badge'])
-#     # pp(settings.platforms.github.badges)
+    pp(settings)
+    # pp(settings.platforms.github.badges)
+    # pp(settings['PLATFORMS']['github']['badges']['size_badge'])
+    # pp(settings['PLATFORMS']['github']['badges']['ci_badge'])
